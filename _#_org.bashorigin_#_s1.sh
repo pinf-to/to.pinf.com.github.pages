841900e6c625a08f0e9dd257494c759f18c3e5c7
Christoph Dorn <christoph@christophdorn.com>
2017-01-31 01:19:23 -0800

    wip

---
+++ _#_org.bashorigin_#_s1.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash.origin.script
+
+depend {
+    "git": "@com.github/bash-origin/bash.origin.gitscm#s1",
+    "boilerplate": "@com.github/pinf-it/it.pinf.com.html5boilerplate#s1"
+}
+
+# @source https://github.com/pinf-to/pinf-to-github-pages/blob/master/bin/pinf-publish.js
+function EXPORTS_publish {
+
+    if ! BO_has_cli_arg "--ignore-dirty" && ! CALL_git is_clean; then
+        BO_exit_error "Your git working directory has uncommitted changes!"
+    fi
+
+    local gitRemoteUrl="$(git config --get remote.origin.url)"
+    if ! BO_test "$gitRemoteUrl" "github\\.com"; then
+        BO_exit_error "The 'origin' of your git remote must point to github!"
+    fi
+
+    local sourceClonePath="$(CALL_git get_git_root)"
+    local pagesClonePath="$__RT_DIRNAME__/targets/_$(BO_replace "$gitRemoteUrl" "^.+\\/([^\\/]+)\$")_$(BO_hash "$gitRemoteUrl")"
+
+
+    CALL_git ensure_cloned_commit "$pagesClonePath" "$gitRemoteUrl" "gh-pages"
+
+
+    pushd "$pagesClonePath" > /dev/null
+
+        # Add source repo as source remote
+        CALL_git ensure_remote "source" "file://$sourceClonePath"
+
+        git clean -d -x -f
+        git fetch source
+        git merge source/master
+
+        CALL_boilerplate copy_minimal_as_base "$@"
+        git add -A . 2> /dev/null || true
+        git commit -m "Updated base template" 2> /dev/null || true
+
+echo "PWD: $(pwd)"
+
+git remote show origin
+
+        git push origin gh-pages
+
+    popd > /dev/null
+
+}
--- examples/01-HelloWorld/main.sh
+++ examples/01-HelloWorld/main.sh
@@ -3,10 +3,18 @@
 echo "TEST_MATCH_IGNORE>>>"
 
 depend {
-    "pages": "@com.github/pinf-to/to.pinf.com.github.pages#strawman"
+    "pages": "@com.github/pinf-to/to.pinf.com.github.pages#s1"
 }
 
-CALL_pages publish
+# TODO: Wrap our index.html file in the boilerplate.
+
+CALL_pages publish {
+    "args": {
+        "anchors": {
+            "body": "<a href='https://pinf-to.github.io/to.pinf.com.github.pages/examples/01-HelloWorld/index.html'>examples/01-HelloWorld</a>"
+        }
+    }
+}
 
 echo "<<<TEST_MATCH_IGNORE"
 
